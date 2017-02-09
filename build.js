c4da88957afa86d02a049e1ee63479b8ded3589e
Keari Eggers <keari@Kearis-MacBook-Air.local>
2017-02-08 20:27:50 -0600

    Get build process working

--- build.js
+++ build.js
@@ -1,1 +1,9 @@
 const fs = require('fs')
+const nunjucks = require('nunjucks')
+const work = require('./src/work')
+
+console.log('building index.html')
+
+nunjucks.configure('src/views')
+const rendered = nunjucks.render('index.html', { work: work })
+fs.writeFile('./dist/index.html', rendered, () => console.log('build complete'))
---
+++ src/work.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    "title": "Project title",
+    "desc": "Project description",
+    "technologies": ["React", "Node", "Webpack"],
+    "imgUrl": "http://placehold.it/250x250"
+  },
+  {
+    "title": "Project title 2",
+    "desc": "Project description 2",
+    "technologies": ["React", "Node", "Webpack"],
+    "imgUrl": "http://placehold.it/250x250"
+  }
+]
