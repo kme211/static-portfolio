3f11aa2a7ddb691ab1b899c303fe7344ba3b653d
Keari Eggers <keari@Kearis-MacBook-Air.local>
2017-02-11 21:13:38 -0600

    Add some portfolio items

--- build.js
+++ build.js
@@ -1,9 +1,11 @@
 const fs = require('fs')
 const nunjucks = require('nunjucks')
-const work = require('./src/work')
+let work = require('./src/work')
+const moment = require('moment');
 
 console.log('building index.html')
 
 nunjucks.configure('src/views')
+work = work.map(item => Object.assign({}, item, { date: moment(item.date).fromNow()}))
 const rendered = nunjucks.render('index.html', { work: work })
 fs.writeFile('./dist/index.html', rendered, () => console.log('build complete'))
