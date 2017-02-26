e3ab7b904c173c3731200229624e8cf1eb5f8ec1
Keari Eggers <keari@Kearis-MacBook-Air.local>
2017-02-26 16:34:01 -0600

    add responsiveness

--- src/work.js
+++ src/work.js
@@ -1,121 +1,131 @@
 module.exports = [{
-    name: "Camper Leaderboard",
-    date: new Date(2016, 6, 3),
-    links: {
-      code: "http://codepen.io/kme211/pen/jAmVVq",
-      demo: "http://codepen.io/kme211/full/jAmVVq"
-    },
-    imgUrl: "http://res.cloudinary.com/ddy54k4ks/image/upload/v1474743468/portfolio/leaderboard.jpg",
-    desc: "Displays the top 100 campers of Free Code Camp.",
-    tags: ["React", "ES2015", "Promises"]
-  },{
-    name: "Markdown Previewer",
-    date: new Date(2016, 5, 25),
-    links: {
-      code: "http://codepen.io/kme211/pen/qNRKNL",
-      demo: "http://codepen.io/kme211/full/qNRKNL"
-    },
-    imgUrl: "https://spaceholder.cc/300x185",
-    desc: "Simple GitHub flavored markdown previewer",
-    tags: ["React", "ES2015"]
-  },{
-    name: "Tic Tac Toe",
-    date: new Date(2016, 3, 6),
-    links: {
-      code: "http://codepen.io/kme211/pen/qZVYqP",
-      demo: "http://codepen.io/kme211/full/qZVYqP"
-    },
-    imgUrl: "http://res.cloudinary.com/ddy54k4ks/image/upload/v1474743468/portfolio/ticTacToe.jpg",
-    desc: "Play Tic Tac Toe against the computer",
-    tags: ["OOP", "ES2015", "SCSS"]
-  },{
-    name: "Shopping List",
-    date: new Date(2016, 1, 15),
-    links: {
-      code: "http://codepen.io/kme211/pen/eJMLaL",
-      demo: "http://codepen.io/kme211/full/eJMLaL"
-    },
-    imgUrl: "https://spaceholder.cc/300x185",
-    desc: "Simple shopping list app",
-    tags: ["ES2015", "SCSS", "jQuery", "Backbone.Events"]
-  },{
-    name: "Wikipedia Viewer",
-    date: new Date(2016, 1, 10),
-    links: {
-      code: "http://codepen.io/kme211/pen/NxERyL",
-      demo: "http://codepen.io/kme211/full/NxERyL"
-    },
-    imgUrl: "http://res.cloudinary.com/ddy54k4ks/image/upload/v1474744155/portfolio/wikipediaSearcher.jpg",
-    desc: "Search for Wikipedia articles",
-    tags: ["ES2015", "SCSS", "jQuery"]
-  },{
-    name: "Free Code Camp Camper News",
-    date: new Date(2016, 0, 31),
-    links: {
-      code: "http://codepen.io/kme211/pen/MKrqbB",
-      demo: "http://codepen.io/kme211/full/MKrqbB/"
-    },
-    imgUrl: "https://spaceholder.cc/300x185",
-    desc: "Stylizes Free Code Camp news stories",
-    tags: ["Handlebars", "Lazy image loading", "ES2015", "SCSS"]
-  }, {
-    name: "Calculator",
-    date: new Date(2015, 10, 8),
-    links: {
-      code: "http://codepen.io/kme211/pen/Kdejvv/",
-      demo: "http://codepen.io/kme211/full/Kdejvv/"
-    },
-    imgUrl: "https://spaceholder.cc/300x185",
-    desc: "Adds, subtracts, multiplies, divides.",
-    tags: ["Vanilla Javascript"]
-  }, {
-    name: "Recipe Box",
-    date: new Date(2015, 10, 27),
-    links: {
-      code: "http://codepen.io/kme211/pen/OyeGQO/",
-      demo: "http://codepen.io/kme211/full/OyeGQO/"
-    },
-    imgUrl: "http://res.cloudinary.com/ddy54k4ks/image/upload/v1474743874/portfolio/recipeBox.jpg",
-    desc: "Uses local storage to save your recipes.",
-    tags: ["Knockout", "jQuery"]
-  }, {
-    name: "Local weather app",
-    date: new Date(2015, 10, 8),
-    links: {
-      code: "http://codepen.io/kme211/pen/vLmvJV",
-      demo: "http://codepen.io/kme211/full/vLmvJV"
-    },
-    imgUrl: "https://spaceholder.cc/300x185",
-    desc: "Uses geolocation to show current weather",
-    tags: ["jQuery", "Responsive", "Open Weathermap API", "Geolocation"]
-  }, {
-    name: "Twitch.tv status app",
-    date: new Date(2015, 10, 29),
-    links: {
-      code: "http://codepen.io/kme211/pen/ojKYOq/",
-      demo: "http://codepen.io/kme211/full/ojKYOq/"
-    },
-    imgUrl: "https://spaceholder.cc/300x185",
-    desc: "Consumes data from the Twitch.tv API to see status of list of users.",
-    tags: ["jQuery", "Handlebars", "Twitch.tv API", "ES2015"]
-  }, {
-    name: "Pomodoro Timer",
-    date: new Date(2015, 9, 17),
-    links: {
-      code: "http://codepen.io/kme211/pen/NGwaLB/",
-      demo: "http://codepen.io/kme211/full/NGwaLB/"
-    },
-    imgUrl: "https://spaceholder.cc/300x185",
-    desc: "Just a regular productivity timer",
-    tags: ["jQuery"]
-  }, {
-    name: "Random Quote Machine",
-    date: new Date(2015, 9, 11),
-    links: {
-      code: "http://codepen.io/kme211/pen/gaxPzR/",
-      demo: "http://codepen.io/kme211/full/gaxPzR/"
-    },
-    imgUrl: "https://spaceholder.cc/300x185",
-    desc: "Just spits out random quotes.",
-    tags: ["Bootstrap", "jQuery"]
-  }]
+  name: "Bender Quotes",
+  date: new Date(2017, 1, 26),
+  links: {
+    code: "https://github.com/kme211/bender-quotes",
+    demo: "http://bender-quotes.netlify.com/"
+  },
+  imgUrl: "http://res.cloudinary.com/ddy54k4ks/image/upload/v1488144233/portfolio/bender-quotes.jpg",
+  desc: "Displays random Bender quotes and animates Bender's eyelids.",
+  tags: ["ES2015", "SCSS", "CSS Images"]
+}, {
+  name: "Camper Leaderboard",
+  date: new Date(2016, 6, 3),
+  links: {
+    code: "http://codepen.io/kme211/pen/jAmVVq",
+    demo: "http://codepen.io/kme211/full/jAmVVq"
+  },
+  imgUrl: "http://res.cloudinary.com/ddy54k4ks/image/upload/v1474743468/portfolio/leaderboard.jpg",
+  desc: "Displays the top 100 campers of Free Code Camp.",
+  tags: ["React", "ES2015", "Promises"]
+}, {
+  name: "Markdown Previewer",
+  date: new Date(2016, 5, 25),
+  links: {
+    code: "http://codepen.io/kme211/pen/qNRKNL",
+    demo: "http://codepen.io/kme211/full/qNRKNL"
+  },
+  imgUrl: "https://spaceholder.cc/300x185",
+  desc: "Simple GitHub flavored markdown previewer",
+  tags: ["React", "ES2015"]
+}, {
+  name: "Tic Tac Toe",
+  date: new Date(2016, 3, 6),
+  links: {
+    code: "http://codepen.io/kme211/pen/qZVYqP",
+    demo: "http://codepen.io/kme211/full/qZVYqP"
+  },
+  imgUrl: "http://res.cloudinary.com/ddy54k4ks/image/upload/v1474743468/portfolio/ticTacToe.jpg",
+  desc: "Play Tic Tac Toe against the computer",
+  tags: ["OOP", "ES2015", "SCSS"]
+}, {
+  name: "Shopping List",
+  date: new Date(2016, 1, 15),
+  links: {
+    code: "http://codepen.io/kme211/pen/eJMLaL",
+    demo: "http://codepen.io/kme211/full/eJMLaL"
+  },
+  imgUrl: "https://spaceholder.cc/300x185",
+  desc: "Simple shopping list app",
+  tags: ["ES2015", "SCSS", "jQuery", "Backbone.Events"]
+}, {
+  name: "Wikipedia Viewer",
+  date: new Date(2016, 1, 10),
+  links: {
+    code: "http://codepen.io/kme211/pen/NxERyL",
+    demo: "http://codepen.io/kme211/full/NxERyL"
+  },
+  imgUrl: "http://res.cloudinary.com/ddy54k4ks/image/upload/v1474744155/portfolio/wikipediaSearcher.jpg",
+  desc: "Search for Wikipedia articles",
+  tags: ["ES2015", "SCSS", "jQuery"]
+}, {
+  name: "Free Code Camp Camper News",
+  date: new Date(2016, 0, 31),
+  links: {
+    code: "http://codepen.io/kme211/pen/MKrqbB",
+    demo: "http://codepen.io/kme211/full/MKrqbB/"
+  },
+  imgUrl: "https://spaceholder.cc/300x185",
+  desc: "Stylizes Free Code Camp news stories",
+  tags: ["Handlebars", "Lazy image loading", "ES2015", "SCSS"]
+}, {
+  name: "Calculator",
+  date: new Date(2015, 10, 8),
+  links: {
+    code: "http://codepen.io/kme211/pen/Kdejvv/",
+    demo: "http://codepen.io/kme211/full/Kdejvv/"
+  },
+  imgUrl: "https://spaceholder.cc/300x185",
+  desc: "Adds, subtracts, multiplies, divides.",
+  tags: ["Vanilla Javascript"]
+}, {
+  name: "Recipe Box",
+  date: new Date(2015, 10, 27),
+  links: {
+    code: "http://codepen.io/kme211/pen/OyeGQO/",
+    demo: "http://codepen.io/kme211/full/OyeGQO/"
+  },
+  imgUrl: "http://res.cloudinary.com/ddy54k4ks/image/upload/v1474743874/portfolio/recipeBox.jpg",
+  desc: "Uses local storage to save your recipes.",
+  tags: ["Knockout", "jQuery"]
+}, {
+  name: "Local weather app",
+  date: new Date(2015, 10, 8),
+  links: {
+    code: "http://codepen.io/kme211/pen/vLmvJV",
+    demo: "http://codepen.io/kme211/full/vLmvJV"
+  },
+  imgUrl: "https://spaceholder.cc/300x185",
+  desc: "Uses geolocation to show current weather",
+  tags: ["jQuery", "Responsive", "Open Weathermap API", "Geolocation"]
+}, {
+  name: "Twitch.tv status app",
+  date: new Date(2015, 10, 29),
+  links: {
+    code: "http://codepen.io/kme211/pen/ojKYOq/",
+    demo: "http://codepen.io/kme211/full/ojKYOq/"
+  },
+  imgUrl: "https://spaceholder.cc/300x185",
+  desc: "Consumes data from the Twitch.tv API to see status of list of users.",
+  tags: ["jQuery", "Handlebars", "Twitch.tv API", "ES2015"]
+}, {
+  name: "Pomodoro Timer",
+  date: new Date(2015, 9, 17),
+  links: {
+    code: "http://codepen.io/kme211/pen/NGwaLB/",
+    demo: "http://codepen.io/kme211/full/NGwaLB/"
+  },
+  imgUrl: "https://spaceholder.cc/300x185",
+  desc: "Just a regular productivity timer",
+  tags: ["jQuery"]
+}, {
+  name: "Random Quote Machine",
+  date: new Date(2015, 9, 11),
+  links: {
+    code: "http://codepen.io/kme211/pen/gaxPzR/",
+    demo: "http://codepen.io/kme211/full/gaxPzR/"
+  },
+  imgUrl: "https://spaceholder.cc/300x185",
+  desc: "Just spits out random quotes.",
+  tags: ["Bootstrap", "jQuery"]
+}]
