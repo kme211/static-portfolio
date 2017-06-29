cc210afa2497037e61783f9ebdd05b76c67e3534
Keari Eggers <kungfu.keari@gmail.com>
2017-06-29 17:34:18 -0500

    add favicon

--- dist/js/main.js
+++ dist/js/main.js
@@ -23,11 +23,11 @@
     } else if (delta < 2 * minute) {
       fuzzy = 'a minute ago'
     } else if (delta < hour) {
-      fuzzy = Math.floor(delta / minute) + ' minutes ago.';
+      fuzzy = Math.floor(delta / minute) + ' minutes ago';
     } else if (Math.floor(delta / hour) == 1) {
-      fuzzy = '1 hour ago.'
+      fuzzy = '1 hour ago'
     } else if (delta < day) {
-      fuzzy = Math.floor(delta / hour) + ' hours ago.';
+      fuzzy = Math.floor(delta / hour) + ' hours ago';
     } else if (delta < day * 2) {
       fuzzy = 'yesterday';
     } else if (delta < week) {
