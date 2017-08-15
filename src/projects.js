02e953a1982a62c88df24e8a167954ff76eced62
Keari Eggers <kungfu.keari@gmail.com>
2017-08-15 13:12:54 -0500

    add movie rater

--- src/projects.js
+++ src/projects.js
@@ -1,111 +1,141 @@
-module.exports = [{
-  name: "SRT Maker",
-  date: "2017-08-06",
-  links: {
-    code: "https://github.com/kme211/srt-maker",
-    gif: "/v1502028846/portfolio/srt-maker-animated.gif"
+module.exports = [
+  {
+    name: "Movie Rater",
+    date: "2017-08-15",
+    links: {
+      code: "https://github.com/kme211/movie-rater",
+      demo: "https://movie-rater.netlify.com/"
+    },
+    imgUrl: "/v1502820500/portfolio/movieRater.png",
+    desc: "Add movies to your watch queue and rate them to add them to your watched queue.",
+    tags: ["React", "Redux", "ES2015"]
   },
-  imgUrl: "/v1502028772/portfolio/srt-maker.png",
-  desc: "A tool for easily creating subtitle files for audio clips.",
-  tags: ["React", "Redux", "Electron"]
-},{
-  name: "Weather app",
-  date: "2017-07-10",
-  links: {
-    code: "https://github.com/kme211/weather-app",
-    demo: "https://kearieggers.com/weather/"
+  {
+    name: "SRT Maker",
+    date: "2017-08-06",
+    links: {
+      code: "https://github.com/kme211/srt-maker",
+      gif: "/v1502028846/portfolio/srt-maker-animated.gif"
+    },
+    imgUrl: "/v1502028772/portfolio/srt-maker.png",
+    desc: "A tool for easily creating subtitle files for audio clips.",
+    tags: ["React", "Redux", "Electron"]
   },
-  imgUrl: "/v1499719784/portfolio/weatherNow.png",
-  desc: "Mobile-first web app for checking the weather.",
-  tags: ["React", "ES2015", "Third-Party API", "Geolocation", "Google Maps", "Canvas"]
-}, {
-  name: "Game of Life",
-  date: "2017-05-22",
-  links: {
-    code: "https://codepen.io/kme211/pen/wdxEad",
-    demo: "https://codepen.io/kme211/full/wdxEad/"
+  {
+    name: "Weather app",
+    date: "2017-07-10",
+    links: {
+      code: "https://github.com/kme211/weather-app",
+      demo: "https://kearieggers.com/weather/"
+    },
+    imgUrl: "/v1499719784/portfolio/weatherNow.png",
+    desc: "Mobile-first web app for checking the weather.",
+    tags: [
+      "React",
+      "ES2015",
+      "Third-Party API",
+      "Geolocation",
+      "Google Maps",
+      "Canvas"
+    ]
   },
-  imgUrl: "/v1498493431/portfolio/gameOfLife.png",
-  desc: "Cellular automaton based off of Conway's Game of Life.",
-  tags: ["React", "ES2015"]
-}, {
-  name: "fodmapr",
-  date: "2017-01-16",
-  links: {
-    code: "https://github.com/kme211/fodmapr",
-    demo: "https://fodmapr.net/"
+  {
+    name: "Game of Life",
+    date: "2017-05-22",
+    links: {
+      code: "https://codepen.io/kme211/pen/wdxEad",
+      demo: "https://codepen.io/kme211/full/wdxEad/"
+    },
+    imgUrl: "/v1498493431/portfolio/gameOfLife.png",
+    desc: "Cellular automaton based off of Conway's Game of Life.",
+    tags: ["React", "ES2015"]
   },
-  imgUrl: "/v1498399970/portfolio/fodmapr.png",
-  desc: "Allows users to quickly check if a food is low/high on the FODMAP spectrum.",
-  tags: ["Service workers", "Offline capability"] 
-},{
-  name: "Bender Quotes",
-  date: "2017-02-26",
-  links: {
-    code: "https://github.com/kme211/bender-quotes",
-    demo: "http://bender-quotes.netlify.com/"
+  {
+    name: "fodmapr",
+    date: "2017-01-16",
+    links: {
+      code: "https://github.com/kme211/fodmapr",
+      demo: "https://fodmapr.net/"
+    },
+    imgUrl: "/v1498399970/portfolio/fodmapr.png",
+    desc: "Allows users to quickly check if a food is low/high on the FODMAP spectrum.",
+    tags: ["Service workers", "Offline capability"]
   },
-  imgUrl: "/v1488392940/portfolio/bender-quotes-cropped.jpg",
-  desc: "Displays random Bender quotes and animates Bender's eyelids.",
-  tags: ["ES2015", "SCSS", "CSS Images"]
-}, {
-  name: "Simon Game",
-  date: "2017-02-17",
-  links: {
-    code: "https://github.com/kme211/simon-game",
-    demo: "http://preacher-jessie-81250.netlify.com/"
+  {
+    name: "Bender Quotes",
+    date: "2017-02-26",
+    links: {
+      code: "https://github.com/kme211/bender-quotes",
+      demo: "http://bender-quotes.netlify.com/"
+    },
+    imgUrl: "/v1488392940/portfolio/bender-quotes-cropped.jpg",
+    desc: "Displays random Bender quotes and animates Bender's eyelids.",
+    tags: ["ES2015", "SCSS", "CSS Images"]
   },
-  imgUrl: "/v1488392316/portfolio/simon-game.jpg",
-  desc: "Digital version of the classic electronic memory game.",
-  tags: ["React", "ES2015"]
-}, {
-  name: "Tic Tac Toe",
-  date: "2016-04-06",
-  links: {
-    code: "http://codepen.io/kme211/pen/qZVYqP",
-    demo: "http://codepen.io/kme211/full/qZVYqP"
+  {
+    name: "Simon Game",
+    date: "2017-02-17",
+    links: {
+      code: "https://github.com/kme211/simon-game",
+      demo: "http://preacher-jessie-81250.netlify.com/"
+    },
+    imgUrl: "/v1488392316/portfolio/simon-game.jpg",
+    desc: "Digital version of the classic electronic memory game.",
+    tags: ["React", "ES2015"]
   },
-  imgUrl: "/v1474743468/portfolio/ticTacToe.jpg",
-  desc: "Play Tic Tac Toe against the computer",
-  tags: ["OOP", "ES2015", "SCSS"]
-}, {
-  name: "Shopping List",
-  date: "2016-02-15",
-  links: {
-    code: "http://codepen.io/kme211/pen/eJMLaL",
-    demo: "http://codepen.io/kme211/full/eJMLaL"
+  {
+    name: "Tic Tac Toe",
+    date: "2016-04-06",
+    links: {
+      code: "http://codepen.io/kme211/pen/qZVYqP",
+      demo: "http://codepen.io/kme211/full/qZVYqP"
+    },
+    imgUrl: "/v1474743468/portfolio/ticTacToe.jpg",
+    desc: "Play Tic Tac Toe against the computer",
+    tags: ["OOP", "ES2015", "SCSS"]
   },
-  imgUrl: "/v1474743874/portfolio/shoppingList.jpg",
-  desc: "Simple shopping list app",
-  tags: ["ES2015", "SCSS", "jQuery", "Backbone.Events"]
-}, {
-  name: "Wikipedia Viewer",
-  date: "2016-02-10",
-  links: {
-    code: "http://codepen.io/kme211/pen/NxERyL",
-    demo: "http://codepen.io/kme211/full/NxERyL"
+  {
+    name: "Shopping List",
+    date: "2016-02-15",
+    links: {
+      code: "http://codepen.io/kme211/pen/eJMLaL",
+      demo: "http://codepen.io/kme211/full/eJMLaL"
+    },
+    imgUrl: "/v1474743874/portfolio/shoppingList.jpg",
+    desc: "Simple shopping list app",
+    tags: ["ES2015", "SCSS", "jQuery", "Backbone.Events"]
   },
-  imgUrl: "/v1474744155/portfolio/wikipediaSearcher.jpg",
-  desc: "Search for Wikipedia articles",
-  tags: ["ES2015", "SCSS", "jQuery", "Third-Party API"]
-}, {
-  name: "Calculator",
-  date: "2015-11-08",
-  links: {
-    code: "http://codepen.io/kme211/pen/Kdejvv/",
-    demo: "http://codepen.io/kme211/full/Kdejvv/"
+  {
+    name: "Wikipedia Viewer",
+    date: "2016-02-10",
+    links: {
+      code: "http://codepen.io/kme211/pen/NxERyL",
+      demo: "http://codepen.io/kme211/full/NxERyL"
+    },
+    imgUrl: "/v1474744155/portfolio/wikipediaSearcher.jpg",
+    desc: "Search for Wikipedia articles",
+    tags: ["ES2015", "SCSS", "jQuery", "Third-Party API"]
   },
-  imgUrl: "/v1474743874/portfolio/calculator.jpg",
-  desc: "Very basic calculator.",
-  tags: ["Vanilla Javascript"]
-}, {
-  name: "Recipe Box",
-  date: "2015-11-27",
-  links: {
-    code: "https://codepen.io/kme211/pen/GqXoEE",
-    demo: "https://codepen.io/kme211/full/GqXoEE"
+  {
+    name: "Calculator",
+    date: "2015-11-08",
+    links: {
+      code: "http://codepen.io/kme211/pen/Kdejvv/",
+      demo: "http://codepen.io/kme211/full/Kdejvv/"
+    },
+    imgUrl: "/v1474743874/portfolio/calculator.jpg",
+    desc: "Very basic calculator.",
+    tags: ["Vanilla Javascript"]
   },
-  imgUrl: "/v1499720388/portfolio/recipeBox-v2.png",
-  desc: "Uses local storage to save your recipes.",
-  tags: ["React", "React Router", "SCSS"]
-}]
+  {
+    name: "Recipe Box",
+    date: "2015-11-27",
+    links: {
+      code: "https://codepen.io/kme211/pen/GqXoEE",
+      demo: "https://codepen.io/kme211/full/GqXoEE"
+    },
+    imgUrl: "/v1499720388/portfolio/recipeBox-v2.png",
+    desc: "Uses local storage to save your recipes.",
+    tags: ["React", "React Router", "SCSS"]
+  }
+];
