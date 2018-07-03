c47f959e35fd1438f8c498a9043afe4b1ce91b41
Keari Eggers <kungfu.keari@gmail.com>
2018-07-02 22:29:48 -0500

    Add padding around social icons in sticky header

--- src/experience.js
+++ src/experience.js
@@ -1,29 +1,27 @@
 module.exports = [
   {
-    companyName: 'Sabre',
-    title: 'Senior Software Developer Contractor',
-    dates: ['Sep 2017', 'Present'],
+    companyName: "Sabre",
+    title: "Senior Software Developer Contractor",
+    dates: ["Sep 2017", "Present"],
     bullets: [
-      'Created many new features with unit tests and automation tests for enterprise SaaS application with React and Redux',
-      'Increased developer efficiency by improving the length of time to run unit tests',
-      'Contributed to custom automation framework in its early stages'
+      "Created many new features with unit tests and automation tests for enterprise SaaS application with React and Redux",
+      "Increased developer efficiency by decreasing the length of time to run unit tests",
+      "Contributed to custom automation framework in its early stages"
     ]
   },
   {
-    companyName: 'MLink Technologies',
-    title: 'Multimedia Developer',
-    dates: ['Jan 2014', 'Sep 2017'],
+    companyName: "MLink Technologies",
+    title: "Multimedia Developer",
+    dates: ["Jan 2014", "Sep 2017"],
     bullets: [
-      'Created many highly interactive and custom e-learnings with HTML5, Javascript, and Adobe Flash',
-      'Created internal tools with Node.js and Electron to increase efficiency'
+      "Created many highly interactive and custom e-learnings with HTML5, Javascript, and Adobe Flash",
+      "Created internal tools with Node.js and Electron to increase efficiency"
     ]
   },
   {
-    companyName: 'University of Washington',
-    title: 'Creative Media Developer',
-    dates: ['Oct 2008', 'Aug 2013'],
-    bullets: [
-      'Created interactive medical-based e-learnings with Adobe Flash'
-    ]
+    companyName: "University of Washington",
+    title: "Creative Media Developer",
+    dates: ["Oct 2008", "Aug 2013"],
+    bullets: ["Created interactive medical-based e-learnings with Adobe Flash"]
   }
 ];
